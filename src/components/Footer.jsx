ed81b37c658aae5e7449252dd997bafcccb6850f
JanLascala <lascala.jan@gmail.com>
2025-03-20 18:02:38 +0100

    footer logo solved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,17 @@
 export default function Footer() {
     return (
-        <p>the footer is footer</p>
+        <>
+            <div class="footer_container">
+                <div class="list_container">
+                    <div>list goes here</div>
+                </div>
+                <img class="footer-logo" src="/img/dc-logo-bg.png" alt="" />
+            </div>
+            <div class="media_container"></div>
+
+
+
+        </>
     )
 
 }
