ea5e540e384f9ca4674f5c136b56354b43c1fc23
JanLascala <lascala.jan@gmail.com>
2025-03-20 15:47:05 +0100

    Basic layout start work on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+// import all componentss
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+function App() {
 
 
-
-
-
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+export default function Header() {
+    return (
+        <div class="container">
+            <img src="/img/dc-logo.png" alt="" />
+
+            <nav>
+                <ul>
+                    <li><a href="/characters">Characters</a></li>
+                    <li><a href="/comics">Comics</a></li>
+                    <li><a href="/movies">Movies</a></li>
+                    <li><a href="/tv">TV</a></li>
+                    <li><a href="/games">Games</a></li>
+                    <li><a href="/collectibles">Collectibles</a></li>
+                    <li><a href="/videos">Videos</a></li>
+                    <li><a href="/fans">Fans</a></li>
+                    <li><a href="/news">News</a></li>
+                    <li><a href="/shop">Shop</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+
+}
