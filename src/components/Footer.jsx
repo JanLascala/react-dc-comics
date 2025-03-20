39f26dd545f2214180005c53c7863b5df17908aa
JanLascala <lascala.jan@gmail.com>
2025-03-20 18:42:46 +0100

    Part 1 footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,60 @@ export default function Footer() {
         <>
             <div class="footer_container">
                 <div class="list_container">
-                    <div>list goes here</div>
+                    <div class="row">
+
+                        <div class="col">
+                            <ul>
+                                <h3>DC COMICS</h3>
+
+                                <li>characters</li>
+                                <li>comics</li>
+                                <li>Movies</li>
+                                <li>tv</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+
+                            <ul>
+                                <h3>SHOP</h3>
+                                <li>Shop Dc</li>
+                                <li>Shop Dc collectibles</li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <ul>
+
+                                <h3>DC</h3>
+                                <li>Terms of use</li>
+                                <li>privacy policy news</li>
+                                <li>Add choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subssciprions</li>
+                                <li>Talent Workshop</li>
+                                <li>Cpsc Certificate</li>
+                                <li>Ratings</li>
+                                <li>Shop here</li>
+                                <li>Contact Us</li>
+
+                            </ul>
+
+                        </div>
+                        <div class="col">
+                            <ul>
+                                <h3>SITES</h3>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+
                 </div>
                 <img class="footer-logo" src="/img/dc-logo-bg.png" alt="" />
             </div>
