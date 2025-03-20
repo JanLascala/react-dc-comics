800553ed7096ea6d110379432b5b7bbdc6fa60da
JanLascala <lascala.jan@gmail.com>
2025-03-20 17:26:57 +0100

    Main completed add shop section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,32 @@
 export default function Main() {
     return (
-        <div class="container_main">
-            <p>content goes here</p>
-        </div>
-    )
+        <>
+            <div class="container_main">
+                <p>content goes here</p>
+            </div>
+            <div class="container_shop">
+                <div class="img_container">
+                    <img src="/img/buy-comics-digital-comics.png" alt="" />
+                    <a> Digital comics </a>
+                </div>
+                <div class="img_container">
+                    <img src="/img/buy-comics-merchandise.png" alt="" />
+                    <a> Dc merchandise  </a>
+                </div>
+                <div class="img_container">
+                    <img src="/img/buy-comics-subscriptions.png" alt="" />
+                    <a> Subscription</a>
+                </div>
+                <div class="img_container">
+                    <img src="/img/buy-comics-shop-locator.png" alt="" />
+                    <a> Comic shop locator</a>
+                </div>
+                <div class="img_container">
+                    <img src="/img/buy-dc-power-visa.svg" alt="" />
+                    <a> Dc power Visa </a>
+                </div>
+            </div>
+        </>
 
+    )
 }
