e7b669d906015c0dd48f1fb46b4049ebcf765fed
JanLascala <lascala.jan@gmail.com>
2025-03-21 14:30:02 +0100

    Add shop component Bonus segment of exe.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,11 @@
+import Shop from "./Shop"
 export default function Main() {
     return (
         <>
             <div className="container_main">
                 <p>content goes here</p>
             </div>
-            <div className="container_shop">
-                <div className="img_container">
-                    <img src="/img/buy-comics-digital-comics.png" alt="" />
-                    <a> Digital comics </a>
-                </div>
-                <div className="img_container">
-                    <img src="/img/buy-comics-merchandise.png" alt="" />
-                    <a> Dc merchandise  </a>
-                </div>
-                <div className="img_container">
-                    <img src="/img/buy-comics-subscriptions.png" alt="" />
-                    <a> Subscription</a>
-                </div>
-                <div className="img_container">
-                    <img src="/img/buy-comics-shop-locator.png" alt="" />
-                    <a> Comic shop locator</a>
-                </div>
-                <div className="img_container">
-                    <img src="/img/buy-dc-power-visa.svg" alt="" />
-                    <a> Dc power Visa </a>
-                </div>
-            </div>
+            <Shop />
         </>
 
     )
---
+++ src/components/Shop.jsx
@@ -0,0 +1,27 @@
+export default function Shop() {
+
+    return (
+        <div className="container_shop">
+            <div className="img_container">
+                <img src="/img/buy-comics-digital-comics.png" alt="" />
+                <a> Digital comics </a>
+            </div>
+            <div className="img_container">
+                <img src="/img/buy-comics-merchandise.png" alt="" />
+                <a> Dc merchandise  </a>
+            </div>
+            <div className="img_container">
+                <img src="/img/buy-comics-subscriptions.png" alt="" />
+                <a> Subscription</a>
+            </div>
+            <div className="img_container">
+                <img src="/img/buy-comics-shop-locator.png" alt="" />
+                <a> Comic shop locator</a>
+            </div>
+            <div className="img_container">
+                <img src="/img/buy-dc-power-visa.svg" alt="" />
+                <a> Dc power Visa </a>
+            </div>
+        </div>
+    )
+}
