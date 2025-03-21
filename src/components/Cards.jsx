360ea5350ef1a7fd422b75420ad05db6ffe9551f
JanLascala <lascala.jan@gmail.com>
2025-03-21 15:56:02 +0100

    Update Cards and ComicsList components to display comic details and enhance layout

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,15 @@
 //data comics
 import comics from "../data/comics"
 
-export default function Cards() {
+export default function Cards({ id }) {
+    const comic = comics[id];
+
     return (
         <>
             <div className="col-2">
-                <img src="" alt="" />
-                <h4>title</h4></div>
+                <img className="cover_img" src={comic.thumb} alt={comic.series} />
+                <h4 className="comics_series">{comic.series}</h4>
+            </div>
 
         </>
     )
--- src/components/ComicsList.jsx
+++ src/components/ComicsList.jsx
@@ -6,7 +6,20 @@ export default function ComicsList() {
 
         <div className="container_main">
             <div className="row">
-                <Cards />
+                <Cards id={0} />
+                <Cards id={1} />
+                <Cards id={2} />
+                <Cards id={3} />
+                <Cards id={4} />
+                <Cards id={5} />
+            </div>
+            <div className="row">
+                <Cards id={6} />
+                <Cards id={7} />
+                <Cards id={8} />
+                <Cards id={9} />
+                <Cards id={10} />
+                <Cards id={11} />
             </div>
 
         </div>
