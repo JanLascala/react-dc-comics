84115e868389cd17fb870804190f87c0cb5e0604
JanLascala <lascala.jan@gmail.com>
2025-03-21 16:29:32 +0100

    Add & style jumbotron

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,10 @@
+export default function Jumbotron() {
+    return (
+        <>
+            <div className="jumbo_img"></div>
+
+            <h2 className="main_title">CURRENT SERIES</h2>
+        </>
+
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,11 @@
+import Jumbotron from "./Jumbotron"
 import ComicsList from "./comicsList"
 import Shop from "./Shop"
+
 export default function Main() {
     return (
         <>
+            <Jumbotron />
             <ComicsList />
             <Shop />
         </>
